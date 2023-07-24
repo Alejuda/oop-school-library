c49a92898adfdc8180e86141228e031e54841975
Alejuda <ignacio.fino@mi.unc.edu.ar>
2023-07-24 14:40:07 -0300

    Fix linters

--- person.rb
+++ person.rb
@@ -1,18 +1,12 @@
 class Person
-  attr_reader :id, :name, :age
-  def initialize (age, name = "Unknown", parent_permission = true)
+  attr_accessor :name, :age
+  attr_reader :id
+
+  def initialize(age, name = 'Unknown', parent_permission: true)
     @id = generate_id
     @name = name
     @age = age
     @parent_permission = parent_permission
-  end
-
-  def name=(new_name)
-    @name = new_name
-  end
-
-  def age=(new_age)
-    @age = new_age
   end
 
   def can_use_services?
@@ -25,7 +19,7 @@ class Person
     @age >= 18
   end
 
-  def generate_id 
+  def generate_id
     timestamp = Time.now.to_i
     random_id = rand(1000..9999)
     id = "#{random_id}#{timestamp}"
