84d58ef82c8522d2bd83d1a03085b1ad7514c654
Alejuda <ignacio.fino@mi.unc.edu.ar>
2023-07-25 10:56:41 -0300

    Fix required change

--- trimmer_decorator.rb
+++ trimmer_decorator.rb
@@ -2,6 +2,7 @@ require_relative 'decorator'
 
 class TrimmerDecorator < Decorator
   def correct_name
-    super.slice(0, 10)
+    name = super
+    name.length > 10 ? name.slice(0, 10) : name
   end
 end
