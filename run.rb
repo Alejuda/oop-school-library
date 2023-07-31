ffb63353b189e92f72bb37e66e01ba03f1057916
MOHAMED GAMIL ELDIMARDASH <gigiyoyo2001@yahoo.com>
2023-07-31 18:42:12 +0300

    Create options methos inside the Run class

--- run.rb
+++ run.rb
@@ -27,4 +27,27 @@ class Run
     end
   end
 
+   # rubocop:disable Metrics/CyclomaticComplexity
+   def options(input)
+    case input
+    when 1
+      @app.list_books
+    when 2
+      @app.list_people
+    when 3
+      @app.create_person
+    when 4
+      @app.create_book
+    when 5
+      @app.create_rental
+    when 6
+      @app.list_rentals
+    when 7
+      puts 'Thanks for using the school library App!'
+      exit
+    else
+      puts 'Invalid Entry'
+    end
+  end
+  # rubocop:enable Metrics/CyclomaticComplexity
 end
