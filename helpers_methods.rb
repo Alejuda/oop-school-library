4020ca12c4d5feebea344f8584f81859ec6a7fc1
Alejuda <ignacio.fino@mi.unc.edu.ar>
2023-08-01 12:18:50 -0300

    Add save_rental method and fix linter errors

--- helpers_methods.rb
+++ helpers_methods.rb
@@ -23,4 +23,45 @@ module SaveData
     check_file('books')
     File.write('./data_files/books.json', JSON.pretty_generate(books_data))
   end
+
+  def save_people
+    people_data = []
+    people.each do |person|
+      person_prop = { id: person.id, name: person.name, age: person.age, json_class: person.json_class }
+      if person.json_class == 'Student'
+        person_prop[:classroom] = person.classroom
+        person_prop[:parent_permission] = person.parent_permission
+      else
+        person_prop[:specialization] = person.specialization
+      end
+
+      people_data << person_prop
+    end
+    return if people_data.empty?
+
+    check_file('people')
+    File.write('./data_files/people.json', JSON.pretty_generate(people_data))
+  end
+
+  def save_rentals(rentals)
+    rental_arr = []
+    rentals.each do |rental|
+      rental_prop = {
+        date: rental.date,
+        title: rental.book.title, author: rental.book.author,
+        id: rental.person.id, name: rental.person.name, age: rental.person.age,
+        type: rental.person.json_class
+      }
+      if rental.person.json_class == 'Student'
+        rental_prop[:classroom] = rental.person.classroom
+      else
+        rental_prop[:specialization] = rental.person.specialization
+      end
+      rental_arr << rental_prop
+    end
+    return if rental_arr.empty?
+
+    check_file('rentals')
+    File.write('./data_files/rentals.json', JSON.pretty_generate(rental_arr))
+  end
 end
