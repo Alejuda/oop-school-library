ba15bd7c78650bf555bf81df9e1b895ac8421e7f
Alejuda <ignacio.fino@mi.unc.edu.ar>
2023-08-02 13:05:06 -0300

    Create book test inside spec folder

---
+++ spec/book_spec.rb
@@ -0,0 +1,18 @@
+require_relative '../book'
+require_relative '../helpers_methods'
+
+describe Book do
+  let(:book) { Book.new('The Book', 'Mohamed') }
+
+  describe '#title' do
+    it 'returns the title' do
+      expect(book.title).to eq('The Book')
+    end
+  end
+
+  describe '#author' do
+    it 'returns the author' do
+      expect(book.author).to eq('Mohamed')
+    end
+  end
+end
